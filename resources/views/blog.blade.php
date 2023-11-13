4ee70bc12aaa29f1200977280bc96c11067ffc21
Shabrinani <nurihsanishabrina1991@gmail.com>
2023-11-13 15:34:33 +0700

    pertemuan 13 Nov

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,12 +1,17 @@
+<!DOCTYPE html>
 <html>
 <head>
+    <title>Tutorial Laravel</title>
 </head>
-
 <body>
     <p>
         Nama : {{$nama}} -
         Alamat : {{$alamat}}  -
         Umur : {{$umur}}
+    </p>
+
+    <a href="/pegawai/{{ date('D M Y') }}">Klik Disini</a>
+
 
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -58,5 +58,13 @@ Route::get('/landingpage', function () {
 });
 
 Route::get('perkalian','App\Http\Controllers\DosenController@index');
-
 Route::get('show','App\Http\Controllers\DosenController@showBlog');
+
+Route::get('/pegawai/{nama}', 'App\Http\Controllers\DosenController@shownama');
+
+Route::get('/formulir', 'App\Http\Controllers\DosenController@formulir');
+Route::post('/formulir/proses', 'App\Http\Controllers\DosenController@proses');
+
+Route::get('/blog', 'App\Http\Controllers\BlogController@home');
+Route::get('/blog/tentang', 'App\Http\Controllers\BlogController@tentang');
+Route::get('/blog/kontak', 'App\Http\Controllers\BlogController@kontak');
